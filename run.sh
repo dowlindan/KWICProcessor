7a82f06a65a569d0a4f6b893647a78b21959a7e5
dowlindan <dowlindan@gmail.com>
2026-02-04 22:18:58 -0500

    Main reads from config.properties file

--- run.sh
+++ run.sh
@@ -9,13 +9,14 @@ OUT_DIR="out"
 MAIN_CLASS="edu.drexel.se311.kwic.Main"
 
 
-if [ $# -lt 1 ]; then
-  echo "Usage: ./run.sh <filename>"
+if [ $# -lt 2 ]; then
+  echo "Usage: ./run.sh <kwic-processing|keyword-search|index-generation> <config-filename>"
   exit 1
 fi
 
-FILENAME="$1"
-
+COMMAND="$1"
+CONFIG_FILENAME="$2"
+    
 # ---- Run ----
 echo "Running $MAIN_CLASS"
-java -cp "$OUT_DIR" "$MAIN_CLASS" "$FILENAME"
+java -cp "$OUT_DIR" "$MAIN_CLASS" "$COMMAND" "$CONFIG_FILENAME"
--- src/main/java/edu/drexel/se311/kwic/Main.java
+++ src/main/java/edu/drexel/se311/kwic/Main.java
@@ -1,30 +1,63 @@
 package edu.drexel.se311.kwic;
 
+import edu.drexel.se311.kwic.fileparsing.AbstractFileParser;
 import edu.drexel.se311.kwic.io.*;
+import edu.drexel.se311.kwic.sorting.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // if (args.length < 1) {
-        //     System.err.println("Usage: java Main <filename>");
-        //     System.exit(1);
-        // }
+        if (args.length < 2) {
+            System.err.println("Usage: (java exec) <kwic-processing|keyword-search|index-generation> <config-filename>");
+            System.exit(1);
+        }
         
-        // String filename = args[0];
+        String filename = args[0];
 
-        OptionReader.readOptions();
+        OptionReader.readOptions("config.properties");
+        String inputFile = OptionReader.getString("InputFileName");
         String inputObjString = OptionReader.getString("Input");
-        InputStrategy inputStrategy = (InputStrategy) OptionReader.getObjectFromStr(inputObjString);
+        String outputObjString = OptionReader.getString("Output");
+        String sortingType = OptionReader.getString("Order");
+        String wordFiltering = OptionReader.getString("WordFiltering");
+        String trivialWords = OptionReader.getString("TrivialWords");
 
-        // InputStrategy input = new ConsoleInput();
-        // OutputStrategy output = new ConsoleOutput();
+        CommandsAsStringListInput cList = new CommandsAsStringListInput();
+        cList.addCommand("kwic-processing");
+        AbstractFileParser fileParser = (AbstractFileParser) OptionReader.getObjectFromKey(inputObjString);
+        OutputStrategy outputStrategy = (OutputStrategy) OptionReader.getObjectFromKey(outputObjString);
+        SortingStrategy sortingStrategy;
+        if ("Ascending".equals(sortingType)) {
+            sortingStrategy = new AlphabeticSorter();
+        } else {
+            System.err.println("Unsupported sorting order");
+            System.exit(1);
+            return;
+        }
 
-        // KWICDriver driver = new KWICDriver(input, output);
-        // int result = driver.loadFile(filename);
-        // if (result == 0) {
-        //     driver.run();
-        // } else {
-        //     System.err.println("Failed to load file.");
-        //     System.exit(1);
-        // }
+        boolean filterWords;
+        if ("Yes".equals(wordFiltering)) {
+            filterWords = true;
+        } else if ("No".equals(wordFiltering)) {
+            filterWords = false;
+        } else {
+            System.err.println("Unsupported word filtering choice");
+            System.exit(1);
+            return;
+        }
+        
+        List<String> trivialWordsList = Arrays.asList(trivialWords.split(","));
+
+        
+
+        KWICDriver driver = new KWICDriver(fileParser, cList, outputStrategy, sortingStrategy, filterWords, trivialWordsList);
+        int result = driver.loadFile(inputFile);
+        if (result == 0) {
+            driver.run();
+        } else {
+            System.err.println("Failed to load file.");
+            System.exit(1);
+        }
     }
 }
